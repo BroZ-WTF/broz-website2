3b884b780a5bba261792762934f027e07423d243
Thomseeen <thwa1@web.de>
2020-07-22 13:11:01 +0200

    updated angular and angular cli

--- broz-backend/setup.py
+++ broz-backend/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wsgibackend',
-    version='1.3.3',
+    version='1.4.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
